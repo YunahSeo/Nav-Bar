29232ef72aea83999f48f961a1aab1fc02b8a144
YunahSeo <0204yoona@mju.ac.kr>
2024-08-15 02:11:58 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const navToggle = document.querySelector(".nav-toggle");
+const navToggle = document.getElementById(".nav-toggle");
 const links = document.querySelector(".links");
 
 navToggle.addEventListener('click', function(){
     links.classList.toggle('show-links');
+});
+
+button.addEventListener('mouseover', () => {
+    dropdown.style.display = 'block'; 
+});
+
+button.addEventListener('mouseout', () => {
+    dropdown.style.display = 'none'; 
 });
